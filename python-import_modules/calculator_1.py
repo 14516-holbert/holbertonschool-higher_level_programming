003951c54b347be6b2fb6d3326a6250b7e0752b4
Bayali <14516@gmail.com>
2026-06-01 14:53:01 +0400

    fdsdfsdf

--- python-import_modules/calculator_1.py
+++ python-import_modules/calculator_1.py
@@ -1,12 +1,12 @@
 def add(a, b):
     """Return the addition of a and b."""
     return (a + b)
-def vurma(a, b):
+def mul(a, b):
     """Return the multiplication of a and b."""
     return a * b
-def bolme(a, b):
+def div(a, b):
     """Return the division of a and b."""
     return a / b
-def cixma(a, b):
+def sub(a, b):
     """Return the subtraction of a and b."""
     return a - b
