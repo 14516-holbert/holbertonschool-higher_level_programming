80e72b5fca4eaf5fbbb7ef93e1fabc9def7acc50
Bayali <14516@gmail.com>
2026-06-01 16:20:41 +0400

    fdgdf

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from sys import argv
+import sys
 if __name__ == "__main__":
     n = len(argv) - 1
 
@@ -10,5 +10,5 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(n))
 
-    for i in range(1, len(argv)):
-        print("{}: {}".format(i, argv[i]))
+    for i in range(1, len(sys.argv)):
+        print("{}: {}".format(i, sys.argv[i]))
