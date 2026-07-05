54934820dd1dbd17f455b5e390a7c754fb8a2f6a
14516-holbert <14516@holbertonstudents.com>
2026-07-05 22:44:15 +0400

    dsadas

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,31 +1,19 @@
 #!/usr/bin/python3
-"""Module for adding command-line arguments to a JSON collection file."""
+"""Adds all arguments to a Python list and saves them to a JSON file."""
 
-from importlib.util import module_from_spec, spec_from_file_location
-from os import path
-from pathlib import Path
-from sys import argv
+import sys
+import os
 
-module_dir = Path(__file__).resolve().parent
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-save_spec = spec_from_file_location(
-    'save_to_json_file', module_dir / '5-save_to_json_file.py')
-save_module = module_from_spec(save_spec)
-save_spec.loader.exec_module(save_module)
-save_to_json_file = save_module.save_to_json_file
+filename = "add_item.json"
 
-load_spec = spec_from_file_location(
-    'load_from_json_file', module_dir / '6-load_from_json_file.py')
-load_module = module_from_spec(load_spec)
-load_spec.loader.exec_module(load_module)
-load_from_json_file = load_module.load_from_json_file
-
-if path.exists('add_item.json'):
-    obj_json_file = load_from_json_file('add_item.json')
+if os.path.exists(filename):
+    my_list = load_from_json_file(filename)
 else:
-    obj_json_file = []
+    my_list = []
 
-for i in range(1, len(argv)):
-    obj_json_file.append(argv[i])
+my_list.extend(sys.argv[1:])
 
-save_to_json_file(obj_json_file, 'add_item.json')
+save_to_json_file(my_list, filename)
